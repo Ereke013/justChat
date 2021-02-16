755cf6183897936923915560ec9beb8ecc1d565b
Aibek <180107091@stu.sdu.edu.kz>
2021-02-16 13:11:05 +0600

    models(Message, Group) added

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-#some text
+class Group(models.Model):
+    name = models.CharField("Group name", max_length=100)
+
+
+# class User(models.Model):
+#     name = models.CharField("full name", max_length=50)
+#     group = models.ForeignKey(Group, on_delete=models.CASCADE)
+#     user_ava = models.CharField("User Photo ", max_length= 250)
+#     # group_id = models.IntegerField()
+
+class Message(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField
+    message_text = models.TextField("Message text")
+
+
+
